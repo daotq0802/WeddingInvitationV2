91de6d9de77a7cd04f1c031892d682dd42c877e4
Quý Đạo <tq.dao0802@gmail.com>
2024-12-20 20:16:31 +0900

    update

--- js/firebase.js
+++ js/firebase.js
@@ -7,23 +7,23 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebas
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB4lAz1K8ajP1RcGMNDDnGKwZcz60_eboY",
-  authDomain: "weddinginvitation-816dd.firebaseapp.com",
-  databaseURL: "https://weddinginvitation-816dd-default-rtdb.firebaseio.com",
-  projectId: "weddinginvitation-816dd",
-  storageBucket: "weddinginvitation-816dd.appspot.com",
-  messagingSenderId: "1009849635487",
-  appId: "1:1009849635487:web:1c49f35ba34b0c679a88aa",
-  measurementId: "G-C6YZ80RSHN",
+    apiKey: "AIzaSyB4lAz1K8ajP1RcGMNDDnGKwZcz60_eboY",
+    authDomain: "weddinginvitation-816dd.firebaseapp.com",
+    databaseURL: "https://weddinginvitation-816dd-default-rtdb.firebaseio.com",
+    projectId: "weddinginvitation-816dd",
+    storageBucket: "weddinginvitation-816dd.appspot.com",
+    messagingSenderId: "1009849635487",
+    appId: "1:1009849635487:web:1c49f35ba34b0c679a88aa",
+    measurementId: "G-C6YZ80RSHN",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 import {
-  getDatabase,
-  set,
-  ref,
+    getDatabase,
+    set,
+    ref,
 } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
 const db = getDatabase();
 let firstName = document.getElementById("first-name");
@@ -35,84 +35,88 @@ let message = document.getElementById("message");
 let attendance = document.getElementsByName("attendance");
 let submit = document.getElementById("submitBtn");
 function findSelection() {
-  var sizes = attendance.length;
-  for (let i = 0; i < sizes; i++) {
-    if (attendance[i].checked == true) {
-      return attendance[i].value;
+    var sizes = attendance.length;
+    for (let i = 0; i < sizes; i++) {
+        if (attendance[i].checked == true) {
+            return attendance[i].value;
+        }
     }
-  }
 }
 
 submit.addEventListener("click", () => {
-  if (
-    email.value == "" ||
-    firstName.value == "" ||
-    lastName.value == "" ||
-    phone.value == "" ||
-    message.value == ""
-  ) {
-    document.querySelector(".notification-modal").classList.add("active");
-    document.querySelector(".notification-modal").classList.add("active");
-    document.querySelector(".notification-modal legend").textContent =
-      "Send Message Fail";
-    document.querySelector(".notification-modal p").textContent =
-      "Phát hiện có mục chưa được cấp thông tin, hãy điền đầy đủ thông tin trước khi gửi!!!";
-  } else {
-    if (findSelection() == "accepts") {
-      set(ref(db, "accept/" + Date.now()), {
-        FullName: firstName.value + lastName.value,
-        Email: email.value,
-        PhoneNo: phone.value,
-        GuestNumber: guest.value,
-        Message: message.value,
-      })
-        .then(() => {
-          Email.send({
-            SecureToken: "94a28672-6cd2-4b2a-a9fc-ff982d7dee9f ",
-            To: "daotq8297@gmail.com",
-            From: "tq.dao0802@gmail.com",
-            Subject:
-              findSelection() == "accepts"
-                ? "Accepts With Pleasure!"
-                : "Declines With Regret",
-            Body: `<h1>Name: <span style='color: red'>${
-              firstName.value + " " + lastName.value
-            }</span></h1>
+    if (
+        email.value == "" ||
+        firstName.value == "" ||
+        lastName.value == "" ||
+        phone.value == "" ||
+        message.value == ""
+    ) {
+        document.querySelector(".notification-modal").classList.add("active");
+        document.querySelector(".notification-modal").classList.add("active");
+        document.querySelector(".notification-modal legend").textContent =
+            "Send Message Fail";
+        document.querySelector(".notification-modal p").textContent =
+            "Phát hiện có mục chưa được cấp thông tin, hãy điền đầy đủ thông tin trước khi gửi!!!";
+    } else {
+        if (findSelection() == "accepts") {
+            set(ref(db, "accept/" + Date.now()), {
+                FullName: firstName.value + lastName.value,
+                Email: email.value,
+                PhoneNo: phone.value,
+                GuestNumber: guest.value,
+                Message: message.value,
+            })
+                .then(() => {
+                    Email.send({
+                        SecureToken: "94a28672-6cd2-4b2a-a9fc-ff982d7dee9f ",
+                        To: "daotq8297@gmail.com",
+                        From: "tq.dao0802@gmail.com",
+                        Subject:
+                            findSelection() == "accepts"
+                                ? "Accepts With Pleasure!"
+                                : "Declines With Regret",
+                        Body: `<h1>Name: <span style='color: red'>${
+                            firstName.value + " " + lastName.value
+                        }</span></h1>
             <i style = "font-size: 18px">Email: <span>${email.value}</span></i>
             <p style = "font-size: 18px">Phone: <span>${phone.value}</span></p>
             <p style = "font-size: 18px">Guest: <span>${guest.value}</span></p>
             <p style = "font-size: 18px">Message: <span>${
-              message.value
+                message.value
             }</span></p>`,
-          });
-          document.querySelector(".notification-modal").classList.add("active");
-          document.querySelector(".notification-modal legend").textContent =
-            "Thank for Attending";
-          document.querySelector(".notification-modal p").textContent =
-            "See you in the party";
-          document.querySelector("form").reset();
-        })
-        .catch((error) => {
-          alert("Đã xảy ra lỗi!!!");
-        });
-    } else {
-      set(ref(db, "decline/" + Date.now()), {
-        FullName: firstName.value + lastName.value,
-        Email: email.value,
-        PhoneNo: phone.value,
-        GuestNumber: guest.value,
-        Message: message.value,
-      })
-        .then(() => {
-          Email.send({
-            SecureToken: "94a28672-6cd2-4b2a-a9fc-ff982d7dee9f ",
-            To: "daotq8297@gmail.com",
-            From: "tq.dao0802@gmail.com",
-            Subject:
-              findSelection() == "accepts"
-                ? "Accepts With Pleasure!"
-                : "Declines With Regret",
-            Body: `<h1>Name: <span style='color: red'>
+                    });
+                    document
+                        .querySelector(".notification-modal")
+                        .classList.add("active");
+                    document.querySelector(
+                        ".notification-modal legend"
+                    ).textContent = "Thank for Attending";
+                    document.querySelector(
+                        ".notification-modal p"
+                    ).textContent = "See you in the party";
+                    document.querySelector("form").reset();
+                })
+                .catch((error) => {
+                    alert("Đã xảy ra lỗi!!!");
+                });
+        } else {
+            set(ref(db, "decline/" + Date.now()), {
+                FullName: firstName.value + lastName.value,
+                Email: email.value,
+                PhoneNo: phone.value,
+                GuestNumber: guest.value,
+                Message: message.value,
+            })
+                .then(() => {
+                    Email.send({
+                        SecureToken: "94a28672-6cd2-4b2a-a9fc-ff982d7dee9f ",
+                        To: "daotq8297@gmail.com",
+                        From: "tq.dao0802@gmail.com",
+                        Subject:
+                            findSelection() == "accepts"
+                                ? "Accepts With Pleasure!"
+                                : "Declines With Regret",
+                        Body: `<h1>Name: <span style='color: red'>
             ${firstName.value + " " + lastName.value}</span></h1>
                 <i style = "font-size: 18px">Email:
                  <span>${email.value}</span></i>
@@ -122,27 +126,32 @@ submit.addEventListener("click", () => {
                 <span>${guest.value}</span></p>
                 <p style = "font-size: 18px">Message: 
                 <span>${message.value}</span></p>`,
-          });
-          document.querySelector(".notification-modal").classList.add("active");
-          document.querySelector(".notification-modal legend").textContent =
-            "See you next time!";
-          document.querySelector(".notification-modal p").textContent =
-            "Thật tiếc khi không thể gặp bạn ở buổi tiệc của chúng mình!!!";
-          document.querySelector("form").reset();
-          setTimeout(() => {
-            document.querySelector(".gift-page").classList.add("active")
-            window.scrollTo(0, 7295);
-          }, 2000);
-        })
-        .catch((error) => {
-          alert("Đã xảy ra lỗi!!!");
-        });
+                    });
+                    document
+                        .querySelector(".notification-modal")
+                        .classList.add("active");
+                    document.querySelector(
+                        ".notification-modal legend"
+                    ).textContent = "See you next time!";
+                    document.querySelector(
+                        ".notification-modal p"
+                    ).textContent =
+                        "Thật tiếc khi không thể gặp bạn ở buổi tiệc của chúng mình!!!";
+                    document
+                            .querySelector(".gift-page")
+                            .classList.add("active");
+                })
+                .catch((error) => {
+                    alert("Đã xảy ra lỗi!!!");
+                });
+        }
     }
-  }
 });
 
 document
-  .querySelector(".notification-modal button")
-  .addEventListener("click", () => {
-    document.querySelector(".notification-modal").classList.remove("active");
-  });
+    .querySelector(".notification-modal button")
+    .addEventListener("click", () => {
+        document
+            .querySelector(".notification-modal")
+            .classList.remove("active");
+    });
--- js/index.js
+++ js/index.js
@@ -1,112 +1,119 @@
 let img = document.querySelectorAll(".home-page .image-slider img");
 let indexImage = 0;
 setInterval(() => {
-  if (indexImage > img.length - 1) {
-    indexImage = 0;
-  }
-  document.querySelector(".image-slider img.active").classList.remove("active");
-  img[indexImage].classList.add("active");
-  indexImage++;
+    if (indexImage > img.length - 1) {
+        indexImage = 0;
+    }
+    document
+        .querySelector(".image-slider img.active")
+        .classList.remove("active");
+    img[indexImage].classList.add("active");
+    indexImage++;
 }, 3000);
 
 let groomInfo = document.querySelector(".infor-click button:nth-child(1)");
 let brideInfo = document.querySelector(".infor-click button:nth-child(2)");
 groomInfo.addEventListener("click", () => {
-  document.querySelector(".each-info .active").classList.remove("active");
-  document
-    .querySelector(".each-info .groom-content-info")
-    .classList.add("active");
+    document.querySelector(".each-info .active").classList.remove("active");
+    document
+        .querySelector(".each-info .groom-content-info")
+        .classList.add("active");
 });
 
 brideInfo.addEventListener("click", () => {
-  document.querySelector(".each-info .active").classList.remove("active");
-  document
-    .querySelector(".each-info .bride-content-info")
-    .classList.add("active");
+    document.querySelector(".each-info .active").classList.remove("active");
+    document
+        .querySelector(".each-info .bride-content-info")
+        .classList.add("active");
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  new FlipDown(1735092000, "bride-countdown").start().ifEnded(() => {
-    document
-      .querySelector(".timer .change-location .active")
-      .classList.remove("active");
-    document.querySelector(".change-location").style.display = "none";
-    document.querySelector(".bride-timer").classList.remove("active");
-    document.querySelector("#groom-time").classList.add("active");
-    document.querySelector(".groom-timer").classList.add("active");
-    document.querySelector(".flipdown").innerHTML = `<h2>Timer End</h2>`;
-  });
-  new FlipDown(1736042400, "groom-countdown").start().ifEnded(() => {
-    document.querySelector(".flipdown").innerHTML = `<h2>Timer End</h2>`;
-  });
+    new FlipDown(1735092000, "bride-countdown").start().ifEnded(() => {
+        document
+            .querySelector(".timer .change-location .active")
+            .classList.remove("active");
+        document.querySelector(".change-location").style.display = "none";
+        document.querySelector(".bride-timer").classList.remove("active");
+        document.querySelector("#groom-time").classList.add("active");
+        document.querySelector(".groom-timer").classList.add("active");
+        document.querySelector(".flipdown").innerHTML = `<h2>Timer End</h2>`;
+    });
+    new FlipDown(1736042400, "groom-countdown").start().ifEnded(() => {
+        document.querySelector(".flipdown").innerHTML = `<h2>Timer End</h2>`;
+    });
 });
 
 let brideTime = document.getElementById("bride-time");
 let groomTime = document.getElementById("groom-time");
 brideTime.addEventListener("click", () => {
-  document
-    .querySelector(".timer .change-location .active")
-    .classList.remove("active");
-  document.querySelector(".groom-timer").classList.remove("active");
-  document.querySelector("#bride-time").classList.add("active");
-  document.querySelector(".bride-timer").classList.add("active");
+    document
+        .querySelector(".timer .change-location .active")
+        .classList.remove("active");
+    document.querySelector(".groom-timer").classList.remove("active");
+    document.querySelector("#bride-time").classList.add("active");
+    document.querySelector(".bride-timer").classList.add("active");
 });
 groomTime.addEventListener("click", () => {
-  document
-    .querySelector(".timer .change-location .active")
-    .classList.remove("active");
-  document.querySelector(".bride-timer").classList.remove("active");
-  document.querySelector("#groom-time").classList.add("active");
-  document.querySelector(".groom-timer").classList.add("active");
+    document
+        .querySelector(".timer .change-location .active")
+        .classList.remove("active");
+    document.querySelector(".bride-timer").classList.remove("active");
+    document.querySelector("#groom-time").classList.add("active");
+    document.querySelector(".groom-timer").classList.add("active");
 });
 window.onload = function () {
-  let originalTitle = document.title;
+    let originalTitle = document.title;
 
-  setInterval(function () {
-    document.title =
-      document.title === originalTitle
-        ? "We Are Getting Married!"
-        : originalTitle;
-  }, 3000);
+    setInterval(function () {
+        document.title =
+            document.title === originalTitle
+                ? "We Are Getting Married!"
+                : originalTitle;
+    }, 3000);
 };
 
-const navLink = document.querySelectorAll(".nav_link");
-const sectionLink = document.querySelectorAll(".section");
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => { 
-  anchor.addEventListener('click', function (e) { 
-    e.preventDefault(); document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' }); }); });
-
-let currentSection = "home";
-// window.addEventListener("scroll", () => {
-//   sectionLink.forEach((section) => {
-//     if (window.scrollY >= section.offsetTop) {
-//       currentSection = section.id;
-//     }
-//   });
-
-  navLink.forEach((nav) => {
-    if (nav.href.includes(currentSection)) {
-      document.querySelector("ul li a.active").classList.remove("active");
-      nav.classList.add("active");
-    }
-  });
-
-function goToBook() {
-  window.scrollTo(0,6218)
-  document.querySelector('header').classList.remove('active')
-}
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document
+            .querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
 
 function goToTop() {
-  window.scrollTo(0,0)
+    window.scrollTo(0, 0);
 }
 
-document.querySelector('.nav-toggle').addEventListener('click',()=>{
-  document.querySelector('header').classList.toggle('active')
-})
+document.querySelector(".nav-toggle").addEventListener("click", () => {
+    document.querySelector("header").classList.toggle("active");
+});
 
-navLink.forEach(nav =>{
-  nav.addEventListener('click', () =>{
-    document.querySelector('header').classList.remove('active')
-  })
+const sections = document.querySelectorAll(".section");
+const navLinks = document.querySelectorAll(".nav_link");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+    navLinks.forEach((link) => {
+        if (link.href.includes(current)) {
+            document.querySelector("ul li a.active").classList.remove("active");
+            link.classList.add("active");
+        }
+    });
+});
+
+let acceptRadio = document.getElementById('accepts')
+let declineRadio = document.getElementById('decline')
+acceptRadio.addEventListener('change', ()=>{
+  document.querySelector('.guestFrag').style.display = 'flex'
+  document.querySelector('.gift-page').classList.remove('active')
+})
+declineRadio.addEventListener('change', ()=>{
+  document.querySelector('.guestFrag').style.display = 'none'
 })
